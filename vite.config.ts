6e145d1ac749dd5dc2e2ea340190ccc26b16907b
Denis Zagvozdin <zagvozdeen@gmail.com>
2026-06-16 23:46:31 +0500

    feat: improve login form

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    // vueDevTools(),
-    tailwindcss(),
-  ],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
